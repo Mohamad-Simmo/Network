beec2d1efc8ac96f1913d69283b3cf12f8a685d5
mohammad semmo <mohammadsemmo81@gmail.com>
2022-07-11 14:38:23 +0300

    add api routes

--- network/urls.py
+++ network/urls.py
@@ -7,5 +7,12 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("profile/<int:user_id>", views.profile, name="profile"),
+
+    # API Routes
+    path("entity/like/<int:entity_id>", views.like, name="like"),
+    path("entity/unlike/<int:entity_id>", views.unlike, name="unlike"),
+    path("follow/<int:user_id>", views.follow, name="follow"),
+    path("unfollow/<int:user_id>", views.unfollow, name="unfollow")
 ]
