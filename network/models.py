9bc4b76efa166a015db741d424138b69d870aae2
mohammad semmo <mohammadsemmo81@gmail.com>
2022-07-11 14:37:54 +0300

    modify tables

--- network/models.py
+++ network/models.py
@@ -5,18 +5,22 @@ from datetime import datetime
 class User(AbstractUser):
     pass
 
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followers")
+
 class Entity(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="poster")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     text = models.TextField()
     date = models.DateTimeField(default=datetime.now())
 
 class Like(models.Model):
-    entity = models.ForeignKey(Entity, on_delete=models.CASCADE, related_name="entity")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="liker")
+    entity = models.ForeignKey(Entity, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
 
 class Post(models.Model):
-    entity = models.ForeignKey(Entity, on_delete=models.CASCADE, related_name="post")
+    entity = models.OneToOneField(Entity, on_delete=models.CASCADE, related_name="post")
 
 class Comment(models.Model):
-    entity = models.ForeignKey(Entity, on_delete=models.CASCADE, related_name="comment")
+    entity = models.OneToOneField(Entity, on_delete=models.CASCADE, related_name="comment")
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post")
