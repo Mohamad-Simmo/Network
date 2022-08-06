6e62095841639139af5279f88724e4f49b42b546
mohammad semmo <mohammadsemmo81@gmail.com>
2022-08-06 19:49:34 +0300

    remove comment

--- network/admin.py
+++ network/admin.py
@@ -5,7 +5,6 @@ from .models import *
 
 admin.site.register(User)
 admin.site.register(Post)
-admin.site.register(Comment)
 admin.site.register(Like)
 admin.site.register(Entity)
 admin.site.register(Follow)
